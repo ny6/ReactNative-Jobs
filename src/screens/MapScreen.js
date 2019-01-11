f6a0135d6fe413c7f5655fe48e0b0f46d3dbd586
Yeshu Kochher <yeshu@outlook.in>
2019-01-11 17:55:15 +0530

    one where add map view

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { MapView } from 'expo';
 
-const MapScreen = () => (
-  <View><Text>Map Screen</Text></View>
-);
+class MapScreen extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09,
+    },
+  }
+
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
+  render() {
+    const { mapLoaded, region: reg } = this.state;
+    if (!mapLoaded) return <View style={{ flex: 1, justifyContent: 'center' }}><ActivityIndicator /></View>;
+
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={reg}
+          onRegionChangeComplete={region => this.setState({ region })}
+        />
+      </View>
+    );
+  }
+}
 
 export default MapScreen;
